356280494e477398b96388092c7393d4f706a14f
George <hdf2135468796@gmail.com>
2025-05-07 20:28:20 +0800

    最初版本

---
+++ Hello World/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Hello_World
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSay = new System.Windows.Forms.Button();
+            this.labHelloWorld = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSay
+            // 
+            this.btnSay.Font = new System.Drawing.Font("新細明體", 20F);
+            this.btnSay.Location = new System.Drawing.Point(12, 52);
+            this.btnSay.Name = "btnSay";
+            this.btnSay.Size = new System.Drawing.Size(270, 117);
+            this.btnSay.TabIndex = 0;
+            this.btnSay.Text = "Say Hello World";
+            this.btnSay.UseVisualStyleBackColor = true;
+            this.btnSay.Click += new System.EventHandler(this.btnSay_Click);
+            // 
+            // labHelloWorld
+            // 
+            this.labHelloWorld.AutoSize = true;
+            this.labHelloWorld.Font = new System.Drawing.Font("新細明體", 20F);
+            this.labHelloWorld.Location = new System.Drawing.Point(12, 291);
+            this.labHelloWorld.Name = "labHelloWorld";
+            this.labHelloWorld.Size = new System.Drawing.Size(93, 34);
+            this.labHelloWorld.TabIndex = 1;
+            this.labHelloWorld.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labHelloWorld);
+            this.Controls.Add(this.btnSay);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSay;
+        private System.Windows.Forms.Label labHelloWorld;
+    }
+}
+
